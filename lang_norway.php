5cb04e037f7ffe0e347daf5677df12de959c2efd
Shelfen <51490713+Shelfen@users.noreply.github.com>
2021-01-22 11:50:13 +0100

    Update lang_norway.php
    Change not finished yet, will update further.

--- lang_norway.php
+++ lang_norway.php
@@ -1,14 +1,14 @@
 <?php
 
 //Registration, Sign-Up
-define("ABI_UI_WELCOME_HEADER", "Sign in to Community Hub");
-define("ABI_UI_WELCOME_HEADER_PWRESET", "Reset your password");
-define("ABI_UI_WELCOME_HEADER_REGISTER", "Create an account");
+define("ABI_UI_WELCOME_HEADER", "Logg inn til Community Hub");
+define("ABI_UI_WELCOME_HEADER_PWRESET", "Glemt passordet ditt?");
+define("ABI_UI_WELCOME_HEADER_REGISTER", "Opprett en konto");
 
-define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Not a member of ABI yet?");
-define("ABI_UI_WELCOME_INFO_PWRESET", "Enter your email to reset your password");
-define("ABI_UI_WELCOME_INFO_CREATE", "Enter your details to create an account");
-define("ABI_UI_WELCOME_INFO_AGREEBOX", "I agree to the");
+define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Ikke medlem av ABI enda?");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Venligst oppgi din e-post for å tilbakestille passord");
+define("ABI_UI_WELCOME_INFO_CREATE", "Venligst oppgi dine detaljer for å opprette en konto");
+define("ABI_UI_WELCOME_INFO_AGREEBOX", "Jeg godtar");
 
 define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "Request sent!");
 define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "If this email address is known on our end, you will receive an email shortly.");
@@ -43,38 +43,38 @@ define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "To create an account, you have t
 define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Register session expired!");
 define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "The register process took too long. Please refresh the page and try again.");
 
-define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Unknown confirmation token!");
-define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Invalid activation token. Please make sure to use the link sent via email or contact ABI support.");
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Ukjent bekreftelses nøkkel!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Feil aktiverings nøkkel. Please make sure to use the link sent via email or contact ABI support.");
 
-define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Username is not allowed!");
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Brukernavnet er ikke lovlig!");
 define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "The username you specified is illegal and can not be used. Usernames may only contain alphanumeric characters, minus signs, underscores and numbers.");
 
-define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Username is taken!");
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Brukernavn er i bruk!");
 define("ABI_UI_WELCOME_INFO_USER_TAKEN", "The username you specified is already in use. Please use a different name.");
 
-define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "Email is taken!");
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "E-post er i bruk!");
 define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Another user account has already been registered using this email address.");
 
 define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Invalid credentials provided!");
 define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Invalid login credentials entered! Message team@abinteractive.net for further help.");
 
-define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Account not activated!");
-define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Your account is currently awaiting confirmation. You can re-send your confirmation mail by clicking the button below.");
-define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Resend confirmation mail");
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Konto ikke aktivert!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Din konto venter på e-post bekreftelse. Du kan sende e-post bekreftelsen din på nytt ved å trykke på knappen under.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Send på nytt e-post bekreftelsen");
 
-define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Request sent!");
-define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Your confirmation email has been re-sent. Please also check your spam and promotional folders.");
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Bekreftelse sendt!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Din e-post bekreftelse har blitt sendt på nytt. Venligst sjekk søppelpost og reklame mapper.");
 
-define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Username");
-define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Email");
-define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Password");
-define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Confirm Password");
+define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Brukernavn");
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "E-post");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Passord");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Bekreft Passord");
 
-define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Sign in");
-define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Sign up");
-define("ABI_UI_WELCOME_BUTTON_RESETPW", "Reset password");
-define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Request reset");
-define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Back to login");
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Logg inn");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Registrer deg");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Tilbakestill Passord");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Tilbakestill");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Gå tilbake");
 
-define("ABI_UI_WELCOME_BOX_TERMS", "Terms & Guidelines");
-define("ABI_UI_WELCOME_BOX_PRIVACY", "Privacy Policy");
+define("ABI_UI_WELCOME_BOX_TERMS", "Betingelser & Retningslinjer");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Personvernerklæring");
