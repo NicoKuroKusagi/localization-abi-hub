aaad75902e3b6ca537f14e1549e3830523db2ce8
Leon Kükel <verwaltung@unit13.de>
2021-01-22 10:52:14 +0100

    Add new language vars

--- lang_korean.php
+++ lang_korean.php
@@ -1,6 +1,5 @@
 <?php
 
-
 //Registration, Sign-Up
 define("ABI_UI_WELCOME_HEADER", "커뮤니티 허브에 로그인");
 define("ABI_UI_WELCOME_HEADER_PWRESET", "암호 재생성");
@@ -10,6 +9,61 @@ define("ABI_UI_WELCOME_INFO_NOACCOUNT", "아직 계정이 없으신가요?");
 define("ABI_UI_WELCOME_INFO_PWRESET", "암호 재생성을 위해서 이메일을 적어주세요.");
 define("ABI_UI_WELCOME_INFO_CREATE", "새 계정을 만들기 위해 아래 내용을 입력해 주세요.");
 define("ABI_UI_WELCOME_INFO_AGREEBOX", "체크시 동의합니다.");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "Request sent!");
+define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "If this email address is known on our end, you will receive an email shortly.");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_COMPLETE", "Password reset processed!");
+define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Your password reset has been processed and your password has been changed.");
+
+define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "Your passwords do not match!");
+define("ABI_UI_WELCOME_INFO_PW_NO_MATCH", "The entered passwords do not match.");
+
+define("ABI_UI_WELCOME_HEADER_PW_TOO_WEAK", "Password too weak!");
+define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Your password is too weak. For your own safety, please use a stronger password.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_INVALID", "Email address invalid!");
+define("ABI_UI_WELCOME_INFO_EMAIL_INVALID", "The entered email address is not valid. Please make sure to check for typos.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_AWAIT_CONFIRM", "Welcome to ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Your user account has been created and is now awaiting confirmation. Please check your email inbox, spam and promotional folders.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_CONFIRMED", "Welcome to ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Your account has been activated and you are now able to log in.");
+
+define("ABI_UI_WELCOME_HEADER_REGISTER_DISABLED", "Registrations disabled!");
+define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "Registrations are disabled at this time.");
+
+define("ABI_UI_WELCOME_HEADER_LOGIN_DISABLED", "Logins disabled!");
+define("ABI_UI_WELCOME_INFO_LOGIN_DISABLED", "Logins are disabled at this time.");
+
+define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "Legal requirements not met!");
+define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "To create an account, you have to accept our guidelines and privacy policy!");
+
+define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Register session expired!");
+define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "The register process took too long. Please refresh the page and try again.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Unknown confirmation token!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Invalid activation token. Please make sure to use the link sent via email or contact ABI support.");
+
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Username is not allowed!");
+define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "The username you specified is illegal and can not be used. Usernames may only contain alphanumeric characters, minus signs, underscores and numbers.");
+
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Username is taken!");
+define("ABI_UI_WELCOME_INFO_USER_TAKEN", "The username you specified is already in use. Please use a different name.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "Email is taken!");
+define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Another user account has already been registered using this email address.");
+
+define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Invalid credentials provided!");
+define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Invalid login credentials entered! Message team@abinteractive.net for further help.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Account not activated!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Your account is currently awaiting confirmation. You can re-send your confirmation mail by clicking the button below.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Resend confirmation mail");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Request sent!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Your confirmation email has been re-sent. Please also check your spam and promotional folders.");
 
 define("ABI_UI_WELCOME_PLACEHOLDER_USER", "유저명");
 define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "이메일");
