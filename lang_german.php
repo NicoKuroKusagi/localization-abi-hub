1429451759dbfd86474cc94c4b20b6ff11cb7ab0
Leon Kükel <verwaltung@unit13.de>
2021-01-22 09:38:18 +0100

    Added german

---
+++ lang_german.php
@@ -0,0 +1,26 @@
+<?php
+
+
+//Registration, Sign-Up
+define("ABI_UI_WELCOME_HEADER", "Beim Community Hub anmelden");
+define("ABI_UI_WELCOME_HEADER_PWRESET", "Passwort zurücksetzen");
+define("ABI_UI_WELCOME_HEADER_REGISTER", "Benutzerkonto erstellen");
+
+define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Noch kein Benutzerkonto?");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Zum zurücksetzen des Passworts, bitte email eingeben.");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Zur erstellung eines Benutzerkontos, bitte daten eingeben.");
+define("ABI_UI_WELCOME_INFO_AGREEBOX", "Ich stimme zu: ");
+
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Benutzername");
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Email");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Passwort");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Passwort bestätigen");
+
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Anmelden");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Konto erstellen");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Passwort zurücksetzen");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Änderung anfragen");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Zurück zum Login");
+
+define("ABI_UI_WELCOME_BOX_TERMS", "Nutzungsbedingungen und Richtlinien");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Datenschutzbestimmungen");
