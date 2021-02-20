022f0b17be8a8f4e149cf147ff57c5a8130b0f05
WccKonig <willem.konig@outlook.com>
2021-02-20 21:53:54 +0100

    add lang_dutch.php

---
+++ lang_dutch.php
@@ -0,0 +1,80 @@
+<?php
+
+//Registration, Sign-Up
+define("ABI_UI_WELCOME_HEADER", "Log in bij de Community Hub");
+define("ABI_UI_WELCOME_HEADER_PWRESET", "Wachtwoord vergeten");
+define("ABI_UI_WELCOME_HEADER_REGISTER", "Maak een account aan");
+
+define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Nog geen lid van ABI?");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Vul uw e-mailadres in om uw wachtwoord opnieuw in te stellen");
+define("ABI_UI_WELCOME_INFO_CREATE", "Vul uw gegevens in om een account aan te maken. Gebruikersnaam mag alleen [a-z] [A-Z] [0-9] [.-_] bevatten");
+define("ABI_UI_WELCOME_INFO_AGREEBOX", "Ja, ik ga akkoord met de");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "Aanvraag verzonden!");
+define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "Als dit e-mailadres bij ons bekend is, ontvangt u binnenkort een e-mail.");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_COMPLETE", "Wachtwoord opnieuw instellen verwerkt!");
+define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Het opnieuw instellen van uw wachtwoord is verwerkt en uw wachtwoord is gewijzigd.");
+
+define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "De wachtwoorden komen niet overeen!");
+define("ABI_UI_WELCOME_INFO_PW_NO_MATCH", "De ingevoerde wachtwoorden komen niet overeen.");
+
+define("ABI_UI_WELCOME_HEADER_PW_TOO_WEAK", "Uw wachtwoord is te zwak!");
+define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Uw wachtwoord is te zwak. Gebruik voor uw eigen veiligheid een sterker wachtwoord.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_INVALID", "E-mailadres ongeldig!");
+define("ABI_UI_WELCOME_INFO_EMAIL_INVALID", "Het ingevoerde e-mailadres is niet geldig. Controleer op typefouten.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_AWAIT_CONFIRM", "Welkom op ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Uw gebruikersaccount is aangemaakt en wacht nu op bevestiging. Controleer uw e-mailinbox, spam en promotiemappen.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_CONFIRMED", "Welkom bij ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Uw account is geactiveerd en u kunt nu inloggen.");
+
+define("ABI_UI_WELCOME_HEADER_REGISTER_DISABLED", "Registraties uitgeschakeld!");
+define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "Registraties zijn op dit moment uitgeschakeld.");
+
+define("ABI_UI_WELCOME_HEADER_LOGIN_DISABLED", "Logins uitgeschakeld!");
+define("ABI_UI_WELCOME_INFO_LOGIN_DISABLED", "Logins zijn op dit moment uitgeschakeld moment.");
+
+define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "Wettelijke vereisten niet vervuld!");
+define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "Om een account aan te maken, moet je onze richtlijnen en privacybeleid accepteren!");
+
+define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Registratie sessie verlopen!");
+define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "Het registratieproces duurde te lang. Vernieuw de pagina en probeer het opnieuw.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Onbekend bevestigingstoken!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Ongeldig activeringstoken. Zorg ervoor dat u de link gebruikt die via e-mail is verzonden of neem contact op met ABI-support.");
+
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Gebruikernaam is niet toegestaan!");
+define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "De gebruikersnaam die u heeft opgegeven is kan niet worden gebruikt. Gebruikersnamen mogen alleen alfanumerieke tekens, mintekens, onderstrepingstekens, punten en cijfers bevatten.");
+
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Gebruikersnaam is in gebruik!");
+define("ABI_UI_WELCOME_INFO_USER_TAKEN", "De gebruikersnaam die je hebt opgegeven, is al in gebruik. Gebruik een andere naam.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "E-mail is in gebruik!");
+define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Er is al een ander gebruikersaccount geregistreerd met dit e-mailadres.");
+
+define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Ongeldige inloggegevens opgegeven!");
+define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Ongeldige inloggegevens ingevoerd! Stuur een bericht naar team@abinteractive.net voor verdere hulp.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Account niet geactiveerd!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Uw account wacht momenteel op bevestiging. U kunt uw bevestigingsmail opnieuw verzenden door op de onderstaande knop te klikken.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Verzend de bevestigingsmail opnieuw");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Aanvraag verzonden!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Uw bevestigingsmail is opnieuw verzonden. Controleer ook uw spam- en promotiefolders.");
+
+define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Gebruikersnaam");
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "E-mailadres");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Wachtwoord");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Herhaal Wachtwoord");
+
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Inloggen");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Maak een account aan");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Wachtwoord vergeten");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Verzenden");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Terug naar Inloggen");
+
+define("ABI_UI_WELCOME_BOX_TERMS", "Voorwaarden en richtlijnen");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Privacyverklaring");
