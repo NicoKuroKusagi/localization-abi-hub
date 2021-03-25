a1f69cf05f52631d4fb0a17732b91ffab576285b
Kurtwa2000 <54406135+Kurtwa2000@users.noreply.github.com>
2021-03-25 16:21:04 +0100

    Create lang_spanish.php

---
+++ lang_spanish.php
@@ -0,0 +1,80 @@
+<?php
+
+//Registration, Sign-Up
+define("ABI_UI_WELCOME_HEADER", "Entrar al centro de comunidad");
+define("ABI_UI_WELCOME_HEADER_PWRESET", "Restablecer su contraseña");
+define("ABI_UI_WELCOME_HEADER_REGISTER", "Crear una cuenta");
+
+define("ABI_UI_WELCOME_INFO_NOACCOUNT", "¿Todavía no eres un miembro de ABI?");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Introduzca su correo electrónico para restablecer su contraseña");
+define("ABI_UI_WELCOME_INFO_CREATE", "Introduzca sus datos para crear una cuenta. Nombre de usuario solo puede contener [a-z] [A-Z] [0-9] [.-_]");
+define("ABI_UI_WELCOME_INFO_AGREEBOX", "Estoy de acuerdo con:");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "¡Solicitud enviada!");
+define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "Si este correo electrónico está registrado en nuestro sitio, recibirás un correo cortamente.");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_COMPLETE", "¡Restablecimiento de contraseña fue procesado!");
+define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Su solicitud ha sido procesada y su contraseña ha sido cambiada.");
+
+define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "¡Sus contraseñas no coinciden!");
+define("ABI_UI_WELCOME_INFO_PW_NO_MATCH", "Las contraseñas introducidas no coinciden.");
+
+define("ABI_UI_WELCOME_HEADER_PW_TOO_WEAK", "¡Su contraseña es muy débil!");
+define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Su contraseña es muy débil. Por su propia seguridad, por favor use una contraseña más fuerte.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_INVALID", "¡Correo electrónico inválido!");
+define("ABI_UI_WELCOME_INFO_EMAIL_INVALID", "El correo electrónico introducido no es valido. Por favor, asegúrese de que no haya errores tipográficos.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_AWAIT_CONFIRM", "¡Bienvenido a ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Su cuenta ha sido creada y espera confirmación. Por favor, verifique su correo, incluyendo su correo no deseado y carpeta de promociones.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_CONFIRMED", "¡Bienvenido a ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Su cuenta ha sido activada y ahora puedes iniciar sesión.");
+
+define("ABI_UI_WELCOME_HEADER_REGISTER_DISABLED", "¡Creación de cuentas deshabilitada!");
+define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "La creación de cuentas esta deshabilitada en este momento.");
+
+define("ABI_UI_WELCOME_HEADER_LOGIN_DISABLED", "¡Inicio de sesión deshabilitado!");
+define("ABI_UI_WELCOME_INFO_LOGIN_DISABLED", "El inicio de sesión esta deshabilitado en este momento.");
+
+define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "¡Requerimientos legales no cumplidos!");
+define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "¡Para crear una cuenta, necesita aceptar nuestras pautas y póliza de privacidad!");
+
+define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "¡Se agotó el tiempo para registrarse!");
+define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "El proceso de registro tomó demasiado tiempo. Por favor, refresque la página e intente de nuevo.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "¡Código de confirmación desconocido!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Código de activación invalido. Por favor asegúrese de usar el enlace enviado por correo electrónico, o contacte al apoyo de ABI.");
+
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "¡Nombre de usuario no permitido!");
+define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "El nombre de usuario no está permitido. Solo se permiten caracteres alfanuméricos, guiones, guiones bajos, puntos y números.");
+
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "¡Nombre de usuario ya está en uso!");
+define("ABI_UI_WELCOME_INFO_USER_TAKEN", "El nombre de usuario que ha escrito ya está en uso. Por favor, use uno diferente.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "¡Correo electrónico está tomado!");
+define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Otra cuenta ha sido registrada con este correo electrónico.");
+
+define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "¡Credenciales invalidas!");
+define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Credenciales inválidas entradas al iniciar sesión. Contacte team@abinteractive.net para ayudarle.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "¡Cuenta no activada!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Su cuenta está esperando confirmación. Puede reenviar su correo de confirmación dándole clic al botón abajo.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Reenviar correo de confirmación.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "¡Solicitud enviada!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Su correo de confirmación ha sido reenviado. Por favor, verifique su correo, incluyendo su correo no deseado y carpeta de promociones.");
+
+define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Nombre de Usuario");
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Correo electrónico");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Contraseña");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Confirmar contraseña");
+
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Iniciar sesión");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Registrarse");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Restablecer contraseña");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Solicitar restablecimiento de contraseña");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "De vuelta a inicio de sesión");
+
+define("ABI_UI_WELCOME_BOX_TERMS", "Términos y pautas");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Póliza de privacidad");
