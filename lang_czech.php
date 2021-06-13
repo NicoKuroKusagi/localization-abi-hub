7d2a427ef53dab350d64b60294b2db6301a56c14
KyderCZ <Kyder@users.noreply.github.com>
2021-06-13 09:07:35 +0200

    Update lang_czech.php

--- lang_czech.php
+++ lang_czech.php
@@ -6,7 +6,7 @@ define("ABI_UI_WELCOME_HEADER_PWRESET", "Resetovat heslo");
 define("ABI_UI_WELCOME_HEADER_REGISTER", "Vytvořit účet");
 
 define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Nejste ještě členem ABI?");
-define("ABI_UI_WELCOME_INFO_PWRESET", "Zadejte svůj email pro resetovní svého hesla");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Zadejte svůj email pro resetování svého hesla");
 define("ABI_UI_WELCOME_INFO_CREATE", "Zadejte své údaje pro vytvoření účtu. Uživatelské jméno může obsahovat pouze [a-z] [A-Z] [0-9] [.-_]");
 define("ABI_UI_WELCOME_INFO_AGREEBOX", "Souhlasím s");
 
@@ -14,22 +14,22 @@ define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "Požadavek odeslán!");
 define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "Pokud tahle emailová adressa je nám známa, obdržíte v okamžiku email.");
 
 define("ABI_UI_WELCOME_HEADER_PWRESET_COMPLETE", "Reset hesla byl zpracován!");
-define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Tvůj reset hesla byl zpracován a tvé heslo bylo zmeněno.");
+define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Váš reset hesla byl zpracován a tvé heslo bylo zmeněno.");
 
-define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "Tvé heslo se neshoduje!");
+define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "Vaše heslo se neshoduje!");
 define("ABI_UI_WELCOME_INFO_PW_NO_MATCH", "Zadané hesla se neshodují.");
 
 define("ABI_UI_WELCOME_HEADER_PW_TOO_WEAK", "Heslo je přiliš slabé!");
-define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Tvé heslo je přiliš slabé. Pro svoji vlastní bezpečnost, prosím použijte silnější heslo.");
+define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Vaše heslo je přiliš slabé. Pro svoji vlastní bezpečnost, prosím použijte silnější heslo.");
 
 define("ABI_UI_WELCOME_HEADER_EMAIL_INVALID", "Emailová adresa je neplatná!");
 define("ABI_UI_WELCOME_INFO_EMAIL_INVALID", "Zadaná emailová adresa je neplatná. Prosím ujistěte se, že jste se nepřekleply.");
 
 define("ABI_UI_WELCOME_HEADER_ACCOUNT_AWAIT_CONFIRM", "Vítejte v ABI!");
-define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Tvůj uživatelský účet byl vytvořen a nyní čeká na potvrzení. Prosím zkontrolujte svůj email and spam složku.");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Váš uživatelský účet byl vytvořen a nyní čeká na potvrzení. Prosím zkontrolujte svůj email a spam složku.");
 
 define("ABI_UI_WELCOME_HEADER_ACCOUNT_CONFIRMED", "Vítejte v ABI!");
-define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Tvůj účet byl aktivován a nyní jsi schopný se přihlásit.");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Váš účet byl aktivován a nyní jste schopni se přihlásit.");
 
 define("ABI_UI_WELCOME_HEADER_REGISTER_DISABLED", "Registrace jsou deaktivovány!");
 define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "Registrace jsou deaktivovány v tuto chvíli.");
@@ -37,44 +37,44 @@ define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "Registrace jsou deaktivovány v
 define("ABI_UI_WELCOME_HEADER_LOGIN_DISABLED", "Přihlasování je deaktivováno!");
 define("ABI_UI_WELCOME_INFO_LOGIN_DISABLED", "Přihlasování je deaktivováno v tuto chvíli");
 
-define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "Legal requirements not met!");
-define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "To create an account, you have to accept our guidelines and privacy policy!");
+define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "Zákonem požadované věci, nejsou splněny!");
+define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "Pro vytvoření účtu, musíte souhlasit s našima podmínkama o používání a ochraně osobních údajů! ");
 
-define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Register session expired!");
-define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "The register process took too long. Please refresh the page and try again.");
+define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Registrační relace vypršela!");
+define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "Registrační proces, trval příliš dlouho. Prosím, obnovte stránku (F5) a zkuste to znovu.");
 
-define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Unknown confirmation token!");
-define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Invalid activation token. Please make sure to use the link sent via email or contact ABI support.");
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Neznámy potvrzovací token!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Neplatný aktivační token. Prosím ujistěte se, že jste použili link z emailu nebo kontaktujte ABI podporu.");
 
-define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Username is not allowed!");
-define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "The username you specified is illegal and can not be used. Usernames may only contain alphanumeric characters, minus signs, underscores, dots and numbers.");
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Uživatelské jméno není povoleno!");
+define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "Uživatelské jméno, které jste zadali, je zakázané a nemůže být použito. Uživatelská jména smí obsahovat pouze alfanumerické znaky, znaménka mínus, podtržítka, tečky a čísla.");
 
-define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Username is taken!");
-define("ABI_UI_WELCOME_INFO_USER_TAKEN", "The username you specified is already in use. Please use a different name.");
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Uživatelské jméno je zabrané!");
+define("ABI_UI_WELCOME_INFO_USER_TAKEN", "Uživatelské jméno ktere jste uvedli, se už používá. Prosím použijte jiné jméno.");
 
-define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "Email is taken!");
-define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Another user account has already been registered using this email address.");
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "Email je zabraný!");
+define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Jiný uživatelský účet, je již registrován s touto emailovou adresou.");
 
-define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Invalid credentials provided!");
-define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Invalid login credentials entered! Message team@abinteractive.net for further help.");
+define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Poskytnuty neplatné údaje!");
+define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Neplatné přihlašovací údaje byly zadány! Pošlete zprávu na team@abinteractive.net pro podrobnější pomoc.");
 
-define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Account not activated!");
-define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Your account is currently awaiting confirmation. You can re-send your confirmation mail by clicking the button below.");
-define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Resend confirmation mail");
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Účet není aktivován!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Váš účet aktuálně čeká na potvrzení. Můžete si znovu poslat, potvrzovací email. Kliknutím na tlačítko dole.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Znovu odeslat potvrzovací email.");
 
-define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Request sent!");
-define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Your confirmation email has been re-sent. Please also check your spam and promotional folders.");
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Požadavek odeslán!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Váš potvrzovací email byl znovu odeslán. Prosím zkontrolujte si také spam složku.");
 
-define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Username");
+define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Uživatelské jméno");
 define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Email");
-define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Password");
-define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Confirm Password");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Heslo");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Potvrzení hesla");
 
-define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Sign in");
-define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Sign up");
-define("ABI_UI_WELCOME_BUTTON_RESETPW", "Reset password");
-define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Request reset");
-define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Back to login");
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Přihlásit se");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Registrovat se");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Resetovat heslo");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Vyžádat reset");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Zpět k přihlášení");
 
-define("ABI_UI_WELCOME_BOX_TERMS", "Terms & Guidelines");
-define("ABI_UI_WELCOME_BOX_PRIVACY", "Privacy Policy");
+define("ABI_UI_WELCOME_BOX_TERMS", "Podmínky používání");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Ochrana osobních údajů");
