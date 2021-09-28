f0573488b44ce7cc16cd7c2e84973672b3917e34
e1on <trohimovkavad@gmail.com>
2021-09-28 19:05:42 +0300

    Create Russian localization

---
+++ lang_russian.php
@@ -0,0 +1,79 @@
+<?php
+
+define("ABI_UI_WELCOME_HEADER", "Войти в Community Hub");
+define("ABI_UI_WELCOME_HEADER_PWRESET", "Сброс пароля");
+define("ABI_UI_WELCOME_HEADER_REGISTER", "Создать учетную запись");
+
+define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Еще не являетесь членом ABI?");
+define("ABI_UI_WELCOME_INFO_PWRESET", "Введите свой email для сброса пароля");
+define("ABI_UI_WELCOME_INFO_CREATE", "Введите свои данные для создания учетной записи. Имя пользователя может содержать только [a-z] [A-Z] [0-9] [.-_]");
+define("ABI_UI_WELCOME_INFO_AGREEBOX", "Я согласен с");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "Запрос отправлен!");
+define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "Если этот адрес электронной почты был зарегистрирован, вы получите письмо в ближайшее время.");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_COMPLETE", "Пароль сброшен!");
+define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Ваш пароль был изменен.");
+
+define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "Пароли не совпадают!");
+define("ABI_UI_WELCOME_INFO_PW_NO_MATCH", "Введенные пароли не совпадают.");
+
+define("ABI_UI_WELCOME_HEADER_PW_TOO_WEAK", "Пароль слишком слабый!");
+define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Ваш пароль слишком слабый. Ииспользуйте более надежный пароль.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_INVALID", "Адрес электронной почты недействителен!");
+define("ABI_UI_WELCOME_INFO_EMAIL_INVALID", "Введенный адрес электронной почты недействителен. Пожалуйста, проверьте, нет ли опечаток.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_AWAIT_CONFIRM", "Добро пожаловать в ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Ваша учетная запись пользователя создана и теперь ожидает подтверждения. Пожалуйста, проверьте почтовый ящик, спам и рекламные папки.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_CONFIRMED", "Добро пожаловать в ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Ваша учетная запись была активирована, и теперь вы можете войти в систему.");
+
+define("ABI_UI_WELCOME_HEADER_REGISTER_DISABLED", "Регистрация отключена!");
+define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "В настоящее время регистрация отключена.");
+
+define("ABI_UI_WELCOME_HEADER_LOGIN_DISABLED", "Авторизация отключены!");
+define("ABI_UI_WELCOME_INFO_LOGIN_DISABLED", "В настоящее время вход в систему отключен.");
+
+define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "Юридические требования не соблюдены!");
+define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "Чтобы создать аккаунт, вы должны принять наши правила и политику конфиденциальности!");
+
+define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Регистрационная сессия истекла!");
+define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "Процесс регистрации занял слишком много времени. Пожалуйста, обновите страницу и повторите попытку.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Неизвестный токен подтверждения!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Неверный токен активации. Пожалуйста, воспользуйтесь ссылкой, отправленной по электронной почте, или обратитесь в службу поддержки ABI.");
+
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Имя пользователя запрещено!");
+define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "Указанное вами имя пользователя является запрещённым и не может быть использовано. Имена пользователей могут содержать только буквы, знаки минус, подчеркивание, точки и цифры..");
+
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "Имя пользователя занято!");
+define("ABI_UI_WELCOME_INFO_USER_TAKEN", "Указанное вами имя пользователя уже используется. Пожалуйста, используйте другое имя.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "Электронная почта занята!");
+define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "По этому адресу электронной почты уже зарегистрирована другая учетная запись.");
+
+define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Предоставлены неверные учетные данные!");
+define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Введены неверные учетные данные для входа в систему! Отправьте сообщение team@abinteractive.net для получения дальнейшей помощи.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Аккаунт не активирован!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "В настоящее время ваша учетная запись ожидает подтверждения. Вы можете повторно отправить письмо с подтверждением, нажав на кнопку ниже.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Повторная отправка письма с подтверждением");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Запрос отправлен!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Ваше письмо с подтверждением было отправлено повторно. Пожалуйста, проверьте также папки "Спам" и "Реклама".");
+
+define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Имя пользователя");
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Email");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Пароль");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Подтвердите пароль");
+
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Войти");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Зарегистрироваться");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Сброс пароля");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Запросить сброс");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Назад к входу в систему");
+
+define("ABI_UI_WELCOME_BOX_TERMS", "Условия и руководство");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Политика конфиденциальности");
