be02882c5cd67bdc6574101cb0d8ff6e28106cf9
Obi1 <benjaminhansson3.com@gmail.com>
2022-10-24 21:38:28 +0200

    Added Swedish

---
+++ lang_swedish.php
@@ -0,0 +1,80 @@
+<?php
+
+//Registration, Sign-Up
+define("ABI_UI_WELCOME_HEADER", "Logga in till Community Hub");
+define("ABI_UI_WELCOME_HEADER_PWRESET", "Återställ ditt lösenord");
+define("ABI_UI_WELCOME_HEADER_REGISTER", "Skapa ett konto");
+
+define("ABI_UI_WELCOME_INFO_NOACCOUNT", "Inte en medlem av ABI en?"); 
+define("ABI_UI_WELCOME_INFO_PWRESET", "Ange ditt email för att återställa ditt lösenord");
+define("ABI_UI_WELCOME_INFO_CREATE", "Ange dina detaljer för att skapa ett konto. Ditt Användarnamn kan bara inehålla [a-z] [A-Z] [0-9] [.-_]"); 
+define("ABI_UI_WELCOME_INFO_AGREEBOX", "Jag godkänner till");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_SENT", "Begäran skickat!");
+define("ABI_UI_WELCOME_INFO_PWRESET_SENT", "Om denna email adressen är känd från vår sida, Så kommer du få ett email inom kort.");
+
+define("ABI_UI_WELCOME_HEADER_PWRESET_COMPLETE", "Lösenord återställning bearbetad!");
+define("ABI_UI_WELCOME_INFO_PWRESET_COMPLETE", "Ditt lösenord återställning har blivit bearbetad och ditt lösenord har blivit ändrat.");
+
+define("ABI_UI_WELCOME_HEADER_PW_NO_MATCH", "Dina lösenord matchar inte!");
+define("ABI_UI_WELCOME_INFO_PW_NO_MATCH", "dom angivna lösenorden matchar inte.");
+
+define("ABI_UI_WELCOME_HEADER_PW_TOO_WEAK", "Lösenord för svagt!");
+define("ABI_UI_WELCOME_INFO_PW_TOO_WEAK", "Ditt lösenord är för svagt. För din egen säkerhet, använd ett starkare lösenord.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_INVALID", "Email adressen är ogiltig!");
+define("ABI_UI_WELCOME_INFO_EMAIL_INVALID", "Den inskrivda email adressen är ogiltig. Kolla att du har skrivit rätt.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_AWAIT_CONFIRM", "Välkommen till ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_AWAIT_CONFIRM", "Ditt användar konto har blivit skapat och is now vämtar på Bekräftelse. Kolla din email inbox, spam och promotional mappar.");
+
+define("ABI_UI_WELCOME_HEADER_ACCOUNT_CONFIRMED", "Välkommen till ABI!");
+define("ABI_UI_WELCOME_INFO_ACCOUNT_CONFIRMED", "Ditt konto har blivit activerat och du kan nu logga in.");
+
+define("ABI_UI_WELCOME_HEADER_REGISTER_DISABLED", "Registrering avstängd!");
+define("ABI_UI_WELCOME_INFO_REGISTER_DISABLED", "Registrering är inte tillgängligt just nu.");
+
+define("ABI_UI_WELCOME_HEADER_LOGIN_DISABLED", "Inloggning avstängd!");
+define("ABI_UI_WELCOME_INFO_LOGIN_DISABLED", "Inloggning är inte tillgängligt just nu.");
+
+define("ABI_UI_WELCOME_HEADER_LEGAL_CHECKBOXES", "Juridiska krav har inte blivit mött!");
+define("ABI_UI_WELCOME_INFO_LEGAL_CHECKBOXES", "För att skapa ett konto, måste du acceptera våra riktlinjer och integritetspolicy!");
+
+define("ABI_UI_WELCOME_HEADER_SECURITY_CSRF", "Registrerings sessionen utgånget!");
+define("ABI_UI_WELCOME_INFO_SECURITY_CSRF", "Registreringsprocessen tog för lång tid. Uppdatera sidan och försök igen.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_TOKEN_UNKNOWN", "Okänd bekräftelse token!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_TOKEN_UNKNOWN", "Ogiltig aktiverings token. Se till att använda länken skickad via email eller kontakta ABI support.");
+
+define("ABI_UI_WELCOME_HEADER_USER_ILLEGAL", "Användarnamnet är inte tillåtet!");
+define("ABI_UI_WELCOME_INFO_USER_ILLEGAL", "Användarnamnet du angav är inte tillåtet och kan inte användas. Användarnamnet får endast innehålla alfanumeriska tecken, minustecken, understreck, punkter och siffror.");
+
+define("ABI_UI_WELCOME_HEADER_USER_TAKEN", "användarnamnet är taget!");
+define("ABI_UI_WELCOME_INFO_USER_TAKEN", "Användarnamnet du angav är redan i användning. Använd ett annat namn.");
+
+define("ABI_UI_WELCOME_HEADER_EMAIL_TAKEN", "Emailet är taget!");
+define("ABI_UI_WELCOME_INFO_EMAIL_TAKEN", "Ett annat konto har redan blivit registrerat med denna email adressen.");
+
+define("ABI_UI_WELCOME_HEADER_CREDS_INVALID", "Ogiltiga användaruppgifter har angetts!");
+define("ABI_UI_WELCOME_INFO_CREDS_INVALID", "Ogiltiga inloggningsuppgifter har angetts! Meddela team@abinteractive.net för ytterligare hjälp.");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_PENDING", "Kontot är inte aktiverat!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_PENDING", "Ditt konto väntar nu på bekräftelse. Du kan skicka ditt bekräftelsemail igen genom att klicka på knappen nedan.");
+define("ABI_UI_WELCOME_BUTTON_CONFIRM_PENDING", "Skicka om e-postbekräftelse");
+
+define("ABI_UI_WELCOME_HEADER_CONFIRM_RESENT", "Förfrågan skickad!");
+define("ABI_UI_WELCOME_INFO_CONFIRM_RESENT", "Ditt bekräftelsemail har skickats på nytt. Kolla ochså genom din email inbox, spam och promotional mappar.");
+
+define("ABI_UI_WELCOME_PLACEHOLDER_USER", "Användarnamn");
+define("ABI_UI_WELCOME_PLACEHOLDER_EMAIL", "Email");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORD", "Lösenord");
+define("ABI_UI_WELCOME_PLACEHOLDER_PASSWORDCONFIRM", "Bekräfta ditt lösenord");
+
+define("ABI_UI_WELCOME_BUTTON_SIGNIN", "Logga in");
+define("ABI_UI_WELCOME_BUTTON_SIGNUP", "Bli Medlem");
+define("ABI_UI_WELCOME_BUTTON_RESETPW", "Återställ lösenord");
+define("ABI_UI_WELCOME_BUTTON_RESETPWSUBMIT", "Begär återställning");
+define("ABI_UI_WELCOME_BUTTON_BACKTOLOGIN", "Tillbaka till login");
+
+define("ABI_UI_WELCOME_BOX_TERMS", "Villkor och riktlinjer");
+define("ABI_UI_WELCOME_BOX_PRIVACY", "Integritetspolicy");
